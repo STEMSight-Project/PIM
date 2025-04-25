36d5f5be30560e59563427a9dcde849616944d8a
patricktran2911 <nguyenphuctran@csus.edu>
2025-04-25 02:03:34 -0700

    renaming

--- Back-End/api_router/router.py
+++ Back-End/api_router/router.py
@@ -4,8 +4,7 @@ from api_router.auth import router as auth_router
 from api_router.doctor import router as doctor_router
 from api_router.medical_history import router as medical_history_router
 from api_router.video import router as video_router
-from api_router.video_streaming_ws import router as video_streaming_router
-from api_router.realtime_offer import router as realtime_offer_router
+from api_router.streaming import router as realtime_offer_router
 from api_router.note import router as note_router
 from api_router.patient_event import router as patient_event_router 
 from api_router.reset_password import router as reset_password_router
@@ -19,7 +18,6 @@ api_router.include_router(auth_router, prefix="/auth", tags = ["Auth"])
 api_router.include_router(medical_history_router, prefix= "/medical-history", tags = ["Medical History"])
 api_router.include_router(doctor_router, prefix="/doctors", tags = ["Doctors"])
 api_router.include_router(video_router, prefix='/videos', tags = ["Videos"])
-api_router.include_router(video_streaming_router, prefix='/video-streaming', tags = ["Video Streaming"])
 api_router.include_router(realtime_offer_router, prefix='/streaming', tags = ["Streaming"])
 api_router.include_router(note_router, prefix="/note", tags=["Notes"])
 api_router.include_router(patient_event_router, prefix="/patient_event", tags=["Patient Events"])
