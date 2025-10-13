f7b925e02c337c120406d7c2ca5f79a8d9dc4b05
phernandez31917 <phernandez4@csus.edu>
2025-10-12 22:27:50 -0700

    IDD-75: Add new account creation flow for doctors via login page
    feat: Add new account creation flow for doctors via login page
    
    - Implemented a new UI page for doctors to create an account if they don't have one
    - Added a "Create Account" option to the main login page, redirecting to the new signup page
    - The signup form collects doctor details and allows selection of specialization via enum list
    - After submission, a verification email is sent to the new user
    - Login is blocked until the email is verified

--- Back-End/api_router/router.py
+++ Back-End/api_router/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from api_router.patient import router as patient_router
 from api_router.auth import router as auth_router
+from api_router.create_login import router as create_login_router
 from api_router.doctor import router as doctor_router
+from api_router.doctor import public_router as public_doctor_router
 from api_router.medical_history import router as medical_history_router
 from api_router.video import router as video_router
 from api_router.streaming import router as realtime_offer_router
@@ -17,10 +19,12 @@ api_router = APIRouter()
 """Guys!! All router should be here!!"""
 api_router.include_router(patient_router, prefix="/patients", tags=["Patients"])
 api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
+api_router.include_router(create_login_router, prefix="/auth", tags=["Auth (Public Registration)"])
 api_router.include_router(
     medical_history_router, prefix="/medical-history", tags=["Medical History"]
 )
 api_router.include_router(doctor_router, prefix="/doctors", tags=["Doctors"])
+api_router.include_router(public_doctor_router, prefix="/doctors", tags=["Doctors (Public)"])
 api_router.include_router(video_router, prefix="/videos", tags=["Videos"])
 api_router.include_router(
     realtime_offer_router, prefix="/streaming", tags=["Streaming"]
